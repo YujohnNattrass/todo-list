b58f0cedd7d4df9fe6fabffce169dd6d16e866a3
Yujohn <yujohn.natt@gmail.com>
2020-05-04 12:28:55 -0700

    adding javascript to todo project

--- todo.rb
+++ todo.rb
@@ -128,8 +128,12 @@ end
 post "/lists/:id/destroy" do
   @list_id = params[:id].to_i 
   session[:lists].delete_at(@list_id)
-  session[:success] = "The list has been deleted."
-  redirect "/lists"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    "/lists"
+  else
+   session[:success] = "The list has been deleted."
+   redirect "/lists"
+  end
 end
 
 # Display Single list
@@ -163,8 +167,12 @@ post "/lists/:list_id/todos/:id/destroy" do
 
    todo_id = params[:id].to_i
    @list[:todos].delete_at(todo_id)
-   session[:success] = "The todo has been deleted"
-   redirect "/lists/#{ @list_id }"
+   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+     status 204
+   else
+     session[:success] = "The todo has been deleted"
+     redirect "/lists/#{ @list_id }"
+   end
 end
 
 # Update the status of a todo
